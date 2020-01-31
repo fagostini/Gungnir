dd0133baed836da70b1a1447044b671f89d81246
fagostini <agostini.federico@gmail.com>
2020-01-31 16:36:53 +0100

    Working version of hamming distance to reference

--- hammingFasta.cu
+++ hammingFasta.cu
@@ -4,19 +4,24 @@
 #include <string.h>
 #include <vector>
 
-#define MAXLEN 50
+#define BLOCKSIZE 1024
+
+#define MAXREF 250000000
+
+#define MAXLEN 40
+#define MISMATCH 9
 
 /*
 Compile:
-   nvcc -arch=sm_61 -o hammingFastaGPU hammingFasta.cu
+   nvcc -arch compute_61 -o hammingFastaGPU hammingFasta.cu
 
 Test: 
    ./hammingFastaGPU query.fa
 
-Test (memcheck)
-   nvprof --print-gpu-trace --device-buffer-size on ./hammingFastaGPU test1k.fa
-   nvprof --print-gpu-trace --device-buffer-size on ./hammingFastaGPU test10k.fa
-   nvprof --print-gpu-trace --device-buffer-size on ./hammingFastaGPU test100k.fa
+   Test (memcheck)
+   nvprof --print-gpu-trace --device-buffer-size on ./hammingFastaGPU test1k.fa test1k.fa
+   nvprof --print-gpu-trace --device-buffer-size on ./hammingFastaGPU test10k.fa test10k.fa
+   nvprof --print-gpu-trace --device-buffer-size on ./hammingFastaGPU test100k.fa test100k.fa
 */
 
 inline
@@ -36,8 +41,10 @@ void distance_hamming( char *str, size_t p_s, int *dist, size_t p_d, int N){
     int index =  blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
     int i, ii;
+    // printf("\n%d %d", index, stride);
     if (index < N ){
         for(i=index; i<N; i+=stride){
+            // printf("\n%d %.40s", i, &str[i*p_s]);
             int* row_dis = (int*)((char*)dist + i*p_d);
             for(ii=0; ii<N; ii++){
                 int count = 0;
@@ -48,13 +55,49 @@ void distance_hamming( char *str, size_t p_s, int *dist, size_t p_d, int N){
                     if( str[i*p_s+pos] != str[ii*p_s+pos] )
                         count++; 
                 }
-                // printf(" %d\n", count);
-                row_dis[ii] = count;
+                // printf("   %d", count);
+                if( count <= MISMATCH ){
+                    row_dis[count] += 1;
+                    // printf("%d %d %d\n", i, count, row_dis[count]);
+                }
             }
             // printf("\n");
         }
     }
     __syncthreads();
+}
+
+__global__
+void distance_hamming2( char *str, size_t p_s, char *ref, size_t len, int *dist, size_t p_d, int N){
+    int index =  blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    int i, ii;
+    // printf("\n%d %d", index, stride);
+    if (index < N ){
+        for(i=index; i<N; i+=stride){
+            // printf("\n%d %.40s %s", i, &str[i*p_s], &ref[0]);
+            int* row_dis = (int*)((char*)dist + i*p_d);
+            for(ii=0; ii<(len-MAXLEN+1); ii++){
+                // printf("\n%d %.40s %.40s", i, &str[i*p_s], &ref[ii]);
+                int count = 0;
+                for(int pos=0; pos<p_s; pos++){
+                    if( str[i*p_s+pos] == '\0' )
+                        break;
+                    // printf("%c", str[i*p_s+pos]);
+                    if( str[i*p_s+pos] != ref[ii+pos] )
+                        count++; 
+                }
+                // printf("\n%d %.40s %.40s %d", i, &str[i*p_s], &ref[ii], count);
+                // printf("   %d", count);
+                if( count <= MISMATCH ){
+                    row_dis[count] += 1;
+                    // printf("%d %d %d\n", i, count, row_dis[count]);
+                }
+            }
+            // printf("\n");
+        }
+    }
+    // __syncthreads();
 }
 
 /* Code from https://www.geeksforgeeks.org/edit-distance-dp-5/ */
@@ -110,13 +153,19 @@ int distance_levenshtein(std::string str1, std::string str2){
 /* Code from http://rosettacode.org/wiki/FASTA_format#C.2B.2B */
 int main( int argc, char **argv ){
     if( argc <= 1 ){
-        std::cerr << "Usage: "<<argv[0]<<" [infile]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <QUERY_FASTA> [<REFERENCE_FASTA>]" << std::endl;
         return -1;
     }
  
     std::ifstream input(argv[1]);
     if( !input.good() ){
         std::cerr << "Error opening '" << argv[1] << "'. Bailing out." << std::endl;
+        return -1;
+    }
+
+    std::ifstream ref(argv[2]);
+    if( !ref.good() ){
+        std::cerr << "Error opening '" << argv[2] << "'. Bailing out." << std::endl;
         return -1;
     }
 
@@ -136,9 +185,9 @@ int main( int argc, char **argv ){
     printf("Allocating 2D arrays on host... ");
     int *dis;
     char *ids, *seq;
-    ids = (char*) malloc(sizeof(char)*(MAXLEN)*N);
-    seq = (char*) malloc(sizeof(char)*(MAXLEN)*N);
-    dis = (int*) calloc(sizeof(int), N*N);
+    ids = (char*) malloc(sizeof(char)*(MAXLEN+1)*N);
+    seq = (char*) malloc(sizeof(char)*(MAXLEN+1)*N);
+    dis = (int*) calloc(sizeof(int), (MISMATCH+1)*N);
     printf("OK\n");	
 
     int i = 0;
@@ -151,22 +200,22 @@ int main( int argc, char **argv ){
         // std::cout << c << std::endl;
         if( c == '>' ){
             int p = 0;
-            char name[(MAXLEN)];
+            char name[(MAXLEN+1)];
             do{
                 name[p++] = c;
                 input.get(c);
-            } while( c != '\n' || p > MAXLEN );
+            } while( c != '\n' || p > (MAXLEN+1) );
             name[p] = '\0';
-            memcpy( &ids[i*MAXLEN], name, sizeof(char)*MAXLEN );
+            memcpy( &ids[i*(MAXLEN+1)], name, sizeof(char)*(MAXLEN+1) );
         } else {
             int p = 0;
-            char content[(MAXLEN)];
+            char content[(MAXLEN+1)];
             do{
                 content[p++] = c;
                 input.get(c);
-            } while( c != '\n' || p > MAXLEN );
+            } while( c != '\n' || p > (MAXLEN+1) );
             content[p] = '\0';
-            memcpy( &seq[i*MAXLEN], content, sizeof(char)*MAXLEN ); 
+            memcpy( &seq[i*(MAXLEN+1)], content, sizeof(char)*(MAXLEN+1) ); 
             i++;
         }
         input.get(c);
@@ -176,10 +225,10 @@ int main( int argc, char **argv ){
 
     // printf("Array initialisation check:\n   ");
     // for (i=0; i<N; i++){
-    //     for(int ii=0; ii<MAXLEN; ii++){
-    //         if( seq[i*MAXLEN+ii] == '\0')
+    //     for(int ii=0; ii<(MAXLEN+1); ii++){
+    //         if( seq[i*(MAXLEN+1)+ii] == '\0')
     //             break;
-    //         std::cout << seq[i*MAXLEN+ii];
+    //         std::cout << seq[i*(MAXLEN+1)+ii];
     //     }
     // printf("\n   ");
     // }
@@ -190,48 +239,104 @@ int main( int argc, char **argv ){
     size_t pitch_seq, pitch_dis;
 
     printf("Allocating 2D arrays on device... ");	
-    checkCuda( cudaMallocPitch((void**)&GPU_seq, &pitch_seq, sizeof(char)*(MAXLEN), N) );
-    checkCuda( cudaMallocPitch((void**)&GPU_dis, &pitch_dis, sizeof(int)*N, N) );
+    checkCuda( cudaMallocPitch((void**)&GPU_seq, &pitch_seq, sizeof(char)*(MAXLEN+1), N) );
+    checkCuda( cudaMallocPitch((void**)&GPU_dis, &pitch_dis, sizeof(int)*(MISMATCH+1), N) );
     printf("OK\n");
 
     printf("Initialising 2D arrays to device... ");	
-    checkCuda( cudaMemcpy2D(GPU_seq, pitch_seq, seq, sizeof(char)*(MAXLEN), sizeof(char)*(MAXLEN), N, cudaMemcpyHostToDevice) );
-    checkCuda( cudaMemcpy2D(GPU_dis, pitch_dis, dis, sizeof(int)*N, sizeof(int)*N, N, cudaMemcpyHostToDevice) );
+    checkCuda( cudaMemcpy2D(GPU_seq, pitch_seq, seq, sizeof(char)*(MAXLEN+1), sizeof(char)*(MAXLEN+1), N, cudaMemcpyHostToDevice) );
+    checkCuda( cudaMemcpy2D(GPU_dis, pitch_dis, dis, sizeof(int)*(MISMATCH+1), sizeof(int)*(MISMATCH+1), N, cudaMemcpyHostToDevice) );
     printf("OK\n");
 
-    int blockSize = 256;
+    printf("Allocating reference array on host... ");
+    char *reference;
+    reference = (char*) malloc(sizeof(char)*(MAXREF+1));
+    printf("OK\n");
+
+    printf("Reading reference the input file... ");
+    int ref_length = 0;
+    ref.get(c);
+    do{
+        if( c == '>' ){
+            do{
+                ref.get(c);
+            } while( c != '\n' );
+        } else {
+            do{
+                reference[ref_length++] = c;
+                ref.get(c);
+            } while( c != '\n' || ref_length > (MAXREF+1) );
+        }
+        ref.get(c);
+    } while( !ref.eof() );
+    reference[ref_length] = '\0';
+    ref.close();
+    printf("OK\n");
+
+    // printf("%s\n", reference);
+
+    char *GPU_ref;
+    size_t pitch_ref = sizeof(char)*ref_length;
+
+    printf("Allocating reference array on device... ");
+    // checkCuda( cudaMallocManaged((void**)&GPU_ref, sizeof(char)*ref_length) );
+    checkCuda( cudaMallocPitch((void**)&GPU_ref, &pitch_ref, sizeof(char)*ref_length, 1) );
+    printf("OK\n");
+
+    printf("Initialising reference array on device... ");
+    checkCuda( cudaMemcpy(GPU_ref, reference, sizeof(char)*ref_length, cudaMemcpyHostToDevice) );
+    printf("OK\n");
+
+    int blockSize = N < BLOCKSIZE ? N : BLOCKSIZE;
     int numBlocks = (N + blockSize - 1) / blockSize;
 
-    printf("Running kernel operations... ");	
-    // multi<<<1, 4>>>(GPU_seq, pitch_seq, GPU_dis, pitch_dis, N);
-    distance_hamming<<<numBlocks, blockSize>>>(GPU_seq, pitch_seq, GPU_dis, pitch_dis, N);
+    printf("Running kernel operations... ");
+    // distance_hamming<<<numBlocks, blockSize>>>(GPU_seq, pitch_seq, GPU_dis, pitch_dis, N);
+    distance_hamming2<<<numBlocks, blockSize>>>(GPU_seq, pitch_seq, GPU_ref, pitch_ref, GPU_dis, pitch_dis, N);
     printf("OK\n");
 
     // Wait for GPU to finish before accessing on host
-    printf("Synchronising host and device... ");	
+    printf("Synchronising host and device... ");
     cudaDeviceSynchronize();
     printf("OK\n");
 
-    printf("Copying 2D array from device to host... ");	
-    cudaMemcpy2D(dis, sizeof(int)*N, GPU_dis, pitch_dis, sizeof(int)*N, N, cudaMemcpyDeviceToHost);
+    printf("Copying 2D array from device to host... ");
+    cudaMemcpy2D(dis, sizeof(int)*(MISMATCH+1), GPU_dis, pitch_dis, sizeof(int)*(MISMATCH+1), N, cudaMemcpyDeviceToHost);
     printf("OK\n");
 
-    // printf("Results check:\n   ");
-    // for(i = 0; i < N*N; i++){
-    //     printf("%d   ", dis[i]);
-    //     if(i%N == N-1)
-    //     printf("\n   ");
-    // }
-    // printf("\n");
+    printf("Writing results...");
+    std::ofstream output("results.tsv");
+    for(i = 0; i < N; i++){
+        for(int ii=0; ii<(MAXLEN+1); ii++){
+            if( ids[i*(MAXLEN+1)+ii] == '\0'){
+                output << '\t';
+                break;
+            }
+            output << ids[i*(MAXLEN+1)+ii];
+        }
+        for(int ii=0; ii<(MAXLEN+1); ii++){
+            if( seq[i*(MAXLEN+1)+ii] == '\0')
+                break;
+            output << seq[i*(MAXLEN+1)+ii];
+        }
+        for(int ii=0; ii<(MISMATCH+1); ii++){
+            output << '\t' << dis[i*(MISMATCH+1)+ii];
+        }
+        output << '\n';
+    }
+    output.close();
+    printf("OK\n");
 
     // release dynamically allocated memory
     printf("Releasing allocated memory:\n");	
     free(ids);
     free(seq);
     free(dis);
+    free(reference);
     printf("Host   --> OK\n");
     cudaFree(GPU_seq);
     cudaFree(GPU_dis);
+    cudaFree(GPU_ref);
     printf("Device --> OK\n");
 
     return 0;
